5be3fe76d0c39126d93837d30804d9ae2f5e4ab0
DanMc7 <adsdan2@hotmail.com>
2026-08-13 16:32:05 -0400

    new colcon build

---
+++ build/tester_py/share/tester_py/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX//home/danmc7/RobotArm/build/tester_py"
